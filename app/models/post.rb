17e640e556d23196ad36cf840967baae0a933d3a
Andrew Snead <andrewcsnead@gmail.com>
2014-11-08 20:17:56 +0000

    deletes test suite photos after run

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.jpg"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+    :default_url => "/images/:style/missing.jpg"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates_attachment_presence :image, message: 'You need to upload a valid image!'
 
