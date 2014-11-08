3bf1717eeb60ded46bc47ebf5c67ca4db37a5427
Andrew Snead <andrewcsnead@gmail.com>
2014-11-08 17:00:10 +0000

    add post model

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,16 +1,37 @@
 require 'rails_helper'
 
 describe 'Posts' do
+
+  before do
+    visit '/'
+  end
   
   context 'no posts have been made' do
     it 'should display a notice that there are no photos to show' do
-      visit '/'
       expect(page).to have_content 'No photos to show'
     end
 
     it 'should have a prompt to upload an image' do
-      visit '/'
       expect(page).to have_link 'Post Something'
     end
   end
+
+  context 'creating posts' do
+
+    before do
+      click_link 'Post Something'
+      expect(Post.count).to eq 0
+    end
+
+    it 'should be able to post by filling in form' do
+      attach_file 'Photo', 'spec/fixtures/images/test.jpg'
+      fill_in 'Caption', with: 'Greatest movie?'
+      click_button 'Post!'
+      expect(page).to have_content 'Greatest movie?'
+      expect(current_path).to eq '/posts'
+      expect(Post.count).to eq 1
+    end
+  end
+
+
 end
